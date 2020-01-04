693490123764076c1de3662714e1c067cece7faf
Darian M <darian.motamed@gmail.com>
2020-01-04 23:10:44 +0100

    next attempt to mute dotenv error

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ silent: process.env.NODE_ENV === 'production' })
+if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 const Discord = require('discord.js')
 const client = new Discord.Client()
 const axios = require('axios')
